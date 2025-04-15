1bbc570434b6124e54b80725cd3aff18323ac733
grizzlywebdev <grizzlyintent@yahoo.com>
2025-04-15 15:50:12 -0500

    finish menu styling

---
+++ whos-who/src/app/components/common/toggle/toggle.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css'],
+})
+export class ToggleComponent implements OnInit {
+  @Input() toggleValue: boolean = false;
+  constructor() {}
+
+  ngOnInit() {}
+}
