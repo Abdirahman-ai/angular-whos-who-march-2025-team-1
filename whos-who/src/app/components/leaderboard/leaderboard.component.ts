b157a4a62c37096b94b828b00ef752002ff6c6a4
grizzlywebdev <grizzlyintent@yahoo.com>
2025-04-16 11:36:42 -0500

    update favicon, update styles on leaderboard

--- whos-who/src/app/components/leaderboard/leaderboard.component.ts
+++ whos-who/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.css'],
-  standalone: true,  // Ensure this is defined for standalone component
-  imports: [CommonModule]  
+  standalone: true, // Ensure this is defined for standalone component
+  imports: [CommonModule],
 })
 export class LeaderboardComponent {
-  leaderboard: { name: string, score: number }[] = [];
+  leaderboard: { name: string; score: number }[] = [];
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     // Load leaderboard from localStorage
-    const storedLeaderboard = JSON.parse(localStorage.getItem('leaderboard') || '[]');
+    const storedLeaderboard = JSON.parse(
+      localStorage.getItem('leaderboard') || '[]'
+    );
 
     // Only keep the top 10 scores
     this.leaderboard = storedLeaderboard.slice(0, 10);
